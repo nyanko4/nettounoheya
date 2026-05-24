fa2345cfef93cd213684a26bbc0d3c3416d7b699
nyanko4 <nyankobetsuaka@outlook.com>
2026-05-24 22:31:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routes/webhook"));
+app.use("/", require("./routes/discord"));
 app.get('/send', (req, res) => {
   res.end(JSON.stringify(process.versions, null, 2));
   console.log("ぬ")
