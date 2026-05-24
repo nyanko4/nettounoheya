f19c4fbea1a2a9850d8b7fd47806d589364e0251
nyanko4 <nyankobetsuaka@outlook.com>
2026-05-24 23:15:39 +0900

    Create discord.js

---
+++ routes/discord.js
@@ -0,0 +1,47 @@
+const { REST, Routes, SlashCommandBuilder, Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('おみくじ')
+    .setDescription('おみくじ'),
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_APITOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.APP_ID),
+      { body: commands }
+    );
+    console.log("Slash commands registered");
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+rest.put(
+  Routes.applicationCommands(process.env.APP_ID),
+  { body: commands }
+);
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  let result = "";
+
+  await interaction.deferReply();
+  if (interaction.commandName == 'おみくじ') {
+    result = await omikuji("discord", interaction);
+  }
+
+  interactin.editReply(result);
+})
