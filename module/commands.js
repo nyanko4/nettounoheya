e8119a1227e87ec98004180974cc7771d73c3660
nyanko4 <nyankobetsuaka@outlook.com>
2026-05-25 21:29:54 +0900

    Create commands.js

---
+++ module/commands.js
@@ -0,0 +1,16 @@
+const { sendchatwork } = require("../ctr/message");
+let result = "";
+
+async function commands(body, messageId, roomId, accountId) {
+  if (body.match(/^おみくじ$/) {
+    result = await omikuji("chatwork", accountId);
+  }
+
+  if (result.trim() !== "") {
+    await sendchatwork(`[aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n${result}`);
+  }
+  
+  return "ok";
+}
+
+module.exports = commands
