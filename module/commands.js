8765a341e06608f40abf3cfc4832f708e9eba60c
nyanko4 <nyankobetsuaka@outlook.com>
2026-05-25 23:25:00 +0900

    Update commands.js

--- module/commands.js
+++ module/commands.js
@@ -8,7 +8,7 @@ async function commands(body, messageId, roomId, accountId) {
   }
 
   if (result.trim() !== "") {
-    await sendchatwork(`[aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n${result}`);
+    await sendchatwork(`[aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n${result}`, roomId);
   }
   
   return "ok";
