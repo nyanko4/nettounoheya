300282f5e28b57add0c890ce7d1b39786b8e75ec
nyanko4 <nyankobetsuaka@outlook.com>
2026-05-25 23:04:40 +0900

    Update discord.js

--- routes/discord.js
+++ routes/discord.js
@@ -5,23 +5,26 @@ https://zenn.dev/semapho/articles/063582c32eff32
 
 const { REST, Routes, SlashCommandBuilder, Client, GatewayIntentBits, Events, MessageFlags } = require('discord.js');
 const { omikuji } = require("../module/omikuji");
-let result = "";
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-client.once(Events.clientReady, () => {
+client.once(Events.ClientReady, () => {
     console.log(`${client.user.tag} ready`);
 });
 
 client.on(Events.MessageCreate, async (message) => {
   if (message.author.bot) return;
+  let result = "";
   console.log(message)
   if (message.content.match(/^おみくじ$/)) {
-    result = await omikuji("discord", message.user.id);
+    result = await omikuji("discord", message.author.id);
   }
-  message.reply(result);
+
+  if (result.trim() !== "") {
+    message.reply(result);
+  }
 })
 
 
@@ -45,13 +48,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_APITOKEN);
   }
 })();
 
-rest.put(
-  Routes.applicationCommands(process.env.APP_ID),
-  { body: commands }
-);
-
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
+  let result = "";
   
   await interaction.deferReply({
     flags: MessageFlags.Ephemeral
@@ -62,3 +61,4 @@ client.on("interactionCreate", async (interaction) => {
   await interaction.editReply({ content: result });
 })
 
+client.login(process.env.DISCORD_APITOKEN);
