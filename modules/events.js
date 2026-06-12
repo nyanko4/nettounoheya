ca4f312ab7df94ac870cd439c053b9d395fa7b41
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-12 21:36:23 +0900

    Create events.js

---
+++ modules/events.js
@@ -0,0 +1,5 @@
+module.exports = {
+  handleMessageCreate: require("../discord/messageCreate"),
+  handleInteractionCreate: require("../discord/interactionCreate"),
+  handleMessageUpdate: require("../discord/messageUpdate"),
+}
