1ed38f5333a7dbd5dd1c36aec97c7b207a786e7c
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-12 21:22:27 +0900

    Update discord.js

--- routes/discord.js
+++ routes/discord.js
@@ -26,14 +26,44 @@ const client = new Client({
   ]
 });
 
+const slashCommands = [
+  new SlashCommandBuilder()
+    .setName("おみくじ")
+    .setDescription("おみくじ"),
+  new SlashCommandBuilder()
+    .setName("要望")
+    .setDescription("botの機能について要望できます")
+    .addStringOption((option) =>
+      option
+        .setName("内容")
+        .setDescription("botの要望したい機能を書いてください")
+        .setRequired(true)),
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_APITOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.APP_ID),
+      { body: slashCommands }
+    );
+    console.log("Slash commands registered");
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
 client.once(Events.ClientReady, () => {
     console.log(`${client.user.tag} ready`);
 });
 
 const handleMessageCreate = require("../discord/messageCreate");
 const handleInteractionCreate = require("../discord/interActionCreate");
+const handleMessageUpdate = require("../discord/messageUpdate
 
 client.on(Events.MessageCreate,　handleMessageCreate);
 client.on(Events.InteractionCreate, handleInteractionCreate);
+client.on(Events.MessageUpdate, handleMessageUpdate);
 
 client.login(process.env.DISCORD_APITOKEN);
