96fffce57b308a1604b12dc5e2a720b664448a09
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-12 21:10:26 +0900

    Create commands.js

---
+++ discord/commands.js
@@ -0,0 +1,22 @@
+const omikuji = require("../module/omikuji");
+
+const commands = {
+  おみくじ: omikuji,
+  debug: debug,
+}
+
+async function executeCommand(message) {
+  const command = commands[message.content];
+
+  if (!command) return;
+
+  try {
+    const result = await command(message, "discord");
+    await message.reply(result);
+  } catch (error) {
+    console.error("executeCommandError", error.response?.data || error.message);
+    await message.reply("エラーが発生しました。");
+  }
+}
+
+module.exports = executeCommand;
