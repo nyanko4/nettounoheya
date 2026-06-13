0b8ef691044fc72af3e6b144e1e2e9b8cbbcc229
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-13 21:37:17 +0900

    Update events.js

--- discord/modules/events.js
+++ discord/modules/events.js
@@ -1,3 +1,3 @@
-import handleMessageCreate from "../discord/messageCreate.js";
-import handleInteractionCreate from "../discord/interactionCreate.js";
-import handleMessageUpdate from "../discord/messageUpdate.js";
+import { handleMessageCreate } from "../discord/messageCreate.js";
+import { handleInteractionCreate } from "../discord/interactionCreate.js";
+import { handleMessageUpdate } from "../discord/messageUpdate.js";
