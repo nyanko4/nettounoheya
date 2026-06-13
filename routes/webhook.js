1de182cc0c2f39a0bc0718af81e367fe6606d3bd
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-13 20:45:28 +0900

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,12 +4,7 @@ const router = express.Router();
 import { mentionWebhook } from "../webhook/mention.js";
 import { getchatWebhook } from "../webhook/getchat.js";
 
-router.post("/mention", async (req, res) => {
-  await mentionWebhook(req, res);
-});
-
-router.post("/getchat", async (req, res) => {
-  await getchatWebhook(req, res);
-});
+router.post("/mention", mentionWebhook);
+router.post("/getchat", getchatWebhook);
 
 export default router;
