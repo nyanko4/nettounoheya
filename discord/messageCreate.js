2156827e84df642d59835ad80a133894e9313a59
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-14 00:05:32 +0900

    Update messageCreate.js

--- discord/messageCreate.js
+++ discord/messageCreate.js
@@ -1,11 +1,15 @@
+//debugFlag
+
 import { executeCommand } from "../discord/modules/commands.js";
 import { logger } from "../discord/modules/utils.js";
 
 export async function handleMessageCreate(message) {
   if (message.author.bot) return;
+  /*
   if (debugFlag) {
     console.log(message);
   }
+  */
   
   console.log(`発言者:${message.author.username}\nメッセージ:${message.content}`);
 
