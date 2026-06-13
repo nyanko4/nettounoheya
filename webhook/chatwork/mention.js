173667ce7f5300ed25d1a35d461a195ec9edf7b1
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-13 20:55:13 +0900

    Update and rename webhook/mention.js to webhook/chatwork/mention.js

--- webhook/chatwork/mention.js
+++ webhook/chatwork/mention.js
@@ -1,6 +1,9 @@
-import { readMessage, deleteMessages, sendMessage } from "../ctr/message.js";
-import { isUserAdmin } from "../ctr/cwdata.js";
-
+import {
+  readMessage,
+  deleteMessages,
+  sendChatwork
+} from "../chatwork/ctr/message.js";
+import { isUserAdmin } from "../chatwork/ctr/cwdata.js";
 
 export async function mentionWebhook(req, res) {
   const {
@@ -14,7 +17,7 @@ export async function mentionWebhook(req, res) {
   
   if (body.includes("削除")) {
     const isAdmin = await isUserAdmin(accountId, roomId);
-    if (!isAdmin) return await sendMessage("管理者のみ利用可能です", roomId, "chatwork");
+    if (!isAdmin) return await sendChatwork("管理者のみ利用可能です", roomId, "chatwork");
     
     await deleteMessages(body, messageId, roomId, accountId, "chatwork");
     return res.sendStatus(200);
