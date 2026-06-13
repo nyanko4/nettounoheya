b11c487e32fa291f39a6ac68076baca0557be3b5
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-13 22:04:30 +0900

    Fix import path for config in messageLogger.js

--- discord/utils/messageLogger.js
+++ discord/utils/messageLogger.js
@@ -1,6 +1,6 @@
 import { EmbedBuilder } from "discord.js";
 
-import { LOG_PERSON_ID, LOG_ROOM_ID } from"../discord/config.js";
+import { LOG_PERSON_ID, LOG_ROOM_ID } from"../config.js";
 
 import { DateTime } from "luxon";
 
