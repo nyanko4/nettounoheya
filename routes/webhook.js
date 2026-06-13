d1bc3173f15ea5800aa3b39c8167c14113f1784f
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-13 21:01:39 +0900

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-import { mentionWebhook } from "../webhook/mention.js";
-import { getchatWebhook } from "../webhook/getchat.js";
+import { mentionWebhook } from "../chatwork/webhook/mention.js";
+import { getchatWebhook } from "../chatwork/webhook/getchat.js";
 
 router.post("/mention", mentionWebhook);
 router.post("/getchat", getchatWebhook);
