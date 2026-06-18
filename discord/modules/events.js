46d55cab58c34a5915fb061bc5ac8b42af17cfe9
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-18 09:26:06 +0900

    Update events.js

--- discord/modules/events.js
+++ discord/modules/events.js
@@ -1,3 +1,5 @@
 export { handleMessageCreate } from "../messageCreate.js";
 export { handleInteractionCreate } from "../interactionCreate.js";
 export { handleMessageUpdate } from "../messageUpdate.js";
+export { handleInputCommand } from "../interactionCreate/inputCommand.js";
+export { handleButton } from "../interactionCreate/button.js";
