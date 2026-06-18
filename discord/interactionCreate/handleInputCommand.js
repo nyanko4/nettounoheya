2082db655f0d4bda62351aab8d77bc44c4bf1822
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-18 09:22:31 +0900

    Create handleInputCommand.js

---
+++ discord/interactionCreate/handleInputCommand.js
@@ -0,0 +1,19 @@
+import { MessageFlags } from "discord.js";
+import { omikuji, requestBotFunction } from "../modules/commands.js";
+
+export async function handleInputCommand(interaction) {
+  let result = "";
+  
+  await interaction.deferReply({
+    flags: MessageFlags.Ephemeral
+  });
+  
+  if (interaction.commandName == "おみくじ") {
+    result = await omikuji(interaction, "discord");
+  }
+  if (interaction.commandName == "要望") {
+    result = await requestBotFunction(interaction);
+  }
+  
+  await interaction.editReply({ content: result });
+}
