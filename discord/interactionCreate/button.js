0473bdee3d68caebc5d4771795a8be721601f257
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-18 09:40:09 +0900

    Create button.js

---
+++ discord/interactionCreate/button.js
@@ -0,0 +1,17 @@
+// buttonを制御する関数
+
+import { confirmButton, cancelButton } from "../../modules/button.js";
+
+const buttonFunction = {
+  confirm: confirmButton,
+  cancel: cancelButton,
+}
+
+export async function handleButton(interaction) {
+  if (!interaction.customId) return;
+  
+  const handler = buttonFunction[interaction.customId];
+  if (!handler) return;
+
+  await handler(interaction)
+}
