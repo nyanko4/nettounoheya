e6daab4c5ffea5a2cd6998d149cc21d85c29c6b0
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-19 21:15:00 +0900

    Update config.js

--- discord/config.js
+++ discord/config.js
@@ -2,3 +2,4 @@ export const BOT_OWNER_ID = process.env.BOT_OWNER_ID;
 export const LOG_PERSON_ID = process.env.LOG_PERSON_ID;
 export const LOG_ROOM_ID = process.env.LOG_ROOM_ID;
 export const REQUEST_ROOM_ID = process.env.REQUEST_ROOM_ID;
+export const CONFIRM_ROOM_ID = process.env.CONFIRM_ROOM_ID;
