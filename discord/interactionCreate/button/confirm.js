cc7d9d63d55ab20bb18b2e6707f8406e852e0f29
nyanko4 <nyankobetsuaka@outlook.com>
2026-06-19 21:30:52 +0900

    Update confirm.js

--- discord/interactionCreate/button/confirm.js
+++ discord/interactionCreate/button/confirm.js
@@ -1,12 +1,9 @@
 // 採用(confirm)buttonが押された際に実行される
 
-import { client } from "../../client.js";
+import { CONFIRM_ROOM_ID } from "../../config.js";
 
 export async function confirmButton(interaction) {
-  await interaction.reply("confirm");
-  const message = await interaction.channel.message.fetch(interaction.message.id);
+  const message = interaction.message.embeds;
   console.log(message);
-  console.log(interaction);
-  console.log(interaction.context);
   return;
 }
